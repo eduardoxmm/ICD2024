caf7e8f6e2ea736ee8c099f28b498375dd35f17a
eduardoxmm <l24141297@queretaro.tecnm.mx>
2024-10-22 12:42:24 -0600

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -1,5 +1,6 @@
 import pytest
 import main as mn
+#import numpy.ma.testutils import assert_almost_equal
 
 def test_hello_world():
     assert mn.hello_world() is True
@@ -8,6 +9,14 @@ def test_get_min():
     resultado1 =mn.get_min([1,2,3])
     assert resultado1 is 1
 
-def test_get_min2():
-    resultado2 =mn.get_min([3,0,-1,8,6])
-    assert resultado2 is -1
+#def test_get_min2():
+#   resultado2 =mn.get_min([3,0,-1,8,6])
+#    assert_almost_equal(resultado2, -9, 1)
+
+def test_get_max1():
+    resultado1 =mn.get_max([3,0,-1,8,6])
+    assert resultado1 is 8
+
+def test_get_max2():
+    resultado2 =mn.get_max([6,0,12,8,1])
+    assert resultado2 is 12
