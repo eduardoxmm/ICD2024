0bfaadb6ff6c2a4c29e87f42b1a0e729639805d0
eduardoxmm <l24141297@queretaro.tecnm.mx>
2024-10-22 12:29:59 -0600

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -5,5 +5,9 @@ def test_hello_world():
     assert mn.hello_world() is True
 
 def test_get_min():
-    resultado =mn.get_min([1,2,3])
-    assert resultado is 1
+    resultado1 =mn.get_min([1,2,3])
+    assert resultado1 is 1
+
+def test_get_min2():
+    resultado2 =mn.get_min([3,0,-1,8,6])
+    assert resultado2 is -1
