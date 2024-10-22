f872ea890571b5ba55208b75d431b148623defe1
eduardoxmm <l24141297@queretaro.tecnm.mx>
2024-10-22 12:30:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from os import write
 def hello_world():
     print("Hello World")
     return True
-hello_world()
 
 def get_min(my_list):
     min_value = my_list[0]
@@ -11,3 +10,6 @@ def get_min(my_list):
         if min_value >= element:
             min_value = element
     return min_value
+
+
+hello_world()
