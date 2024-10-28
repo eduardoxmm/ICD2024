433a8068796c8ca427653cb1272ebf1d6b7c034b
eduardoxmm <l24141297@queretaro.tecnm.mx>
2024-10-27 20:09:10 -0600

    Examen

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from os import write
 
-def gorditas():
-    print("HOLA VENDO GORDITAS:) Y TAMALES:)")
-    return True
-
 def hello_world():
     print("Hello World")
     return True
@@ -23,5 +19,38 @@ def get_max(my_list):
     return max_value
 
 
+def get_fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    fib_sequence = [0, 1]
+    for i in range(2, n):
+        next_value = fib_sequence[i - 1] + fib_sequence[i - 2]
+        fib_sequence.append(next_value)
+    return fib_sequence[:n]
+
+def comun_lists(list1, list2):
+    common_elements = []
+    for element in list1:
+        if element in list2 and element not in common_elements:
+            common_elements.append(element)
+    common_elements.sort()
+    return common_elements
+
+def goldbach(n):
+    def prime(num):
+        if num < 2:
+            return False
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+    if n <= 2 or n % 2 != 0:
+        return False
+    for i in range(2, n):
+        if prime(i) and prime(n - i):
+            return f"{i} + {n - i}"
+    return False
+
 hello_world()
-gorditas()
--- pruebas.py
+++ pruebas.py
@@ -1,20 +1,17 @@
 import pytest
 import main as mn
-import numpy.ma.testutils import assert_almost_equal
+from numpy.testing import assert_almost_equal
 
 def test_hello_world():
     assert mn.hello_world() is True
-
-def test_gorditas():
-    assert mn.gorditas() is True
 
 def test_get_min():
     resultado1 =mn.get_min([1,2,3])
     assert resultado1 == 1
 
-#def test_get_min2():
-#   resultado2 =mn.get_min([3,0,-1,8,6])
-#    assert_almost_equal(resultado2, -9, 1)
+def test_get_min2():
+   resultado2 =mn.get_min([3,0,-9,8,6])
+   assert_almost_equal(resultado2, -9, 1)
 
 def test_get_max1():
     resultado1 =mn.get_max([3,0,-1,8,6])
@@ -23,3 +20,16 @@ def test_get_max1():
 def test_get_max2():
     resultado2 =mn.get_max([6,0,12,8,1])
     assert resultado2 == 12
+
+def test_fibonacci():
+    fibonacci =mn.get_fibonacci(5)
+    assert fibonacci == [0, 1, 1, 2, 3]
+
+def test_comun_list():
+    comun =mn.comun_lists([6,0,12,8,1], [1,3,6,24,12,8])
+    assert comun == [1,6,8,12]
+
+def test_goldbach():
+    gold =mn.goldbach(28)
+    assert gold == "5 + 23"
+
