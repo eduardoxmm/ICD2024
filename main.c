0cb0cece9a82c16e95ce1868719dca7b51ea0643
eduardoxmm <l24141297@queretaro.tecnm.mx>
2024-11-04 17:18:19 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
-int main(void)
-{
-    printf("Hello, World!\n");
-    return 0;
+int main(){
+int h=0, m=0, s=0;
+int reloj=1;
+  do
+  {
+    {
+      while(h<24){
+        m=0;
+        while(m<60){
+          s=0;
+          while(s<60){
+            s=s+1;
+            printf("%02d:%02d:%02d\n",h,m,s);
+          }
+          m=m+1;
+          printf("%02d:%02d:%02d\n",h,m,s);
+        }
+        h=h+1;
+        printf("%02d:%02d:%02d\n",h,m,s);
+      }
+    }
+  }while(reloj==1);
+return 0;
 }
