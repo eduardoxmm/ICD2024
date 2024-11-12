22531d4eaae106dd84067a4ccb07cf76b7654b21
eduardoxmm <l24141297@queretaro.tecnm.mx>
2024-11-12 12:39:04 -0600

    Update Diabetes.py

--- Diabetes.py
+++ Diabetes.py
@@ -2,14 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-workbook1= 'diabetesnorm.xlsx'
+
+workbook1 = 'diabetesnorm.xlsx'
 df = pd.read_excel(workbook1)
-#print(df.head())
-# 1. Histograma de la distribución de edades
-plt.figure(figsize=(10, 6))
-plt.hist(df['age'], bins=10, color='skyblue', alpha=0.7)
-plt.title('Distribución de Edad', fontsize=14)
-plt.xlabel('Edad', fontsize=12)
-plt.ylabel('Frecuencia', fontsize=12)
-plt.grid(True)
-plt.show()
+
+sns.set(style="whitegrid")
+
+for column in df.select_dtypes(include=np.number).columns:
+    plt.figure(figsize=(10, 6))
+    sns.histplot(df[column], bins=10, kde=True, color='skyblue', alpha=0.7)
+
+    plt.title(f'Distribución de {column}', fontsize=16, fontweight='bold')
+    plt.xlabel(column, fontsize=14)
+    plt.ylabel('Frecuencia', fontsize=14)
+
+    plt.grid(visible=True, linestyle='--', linewidth=0.5, alpha=0.7)
+
+    plt.show()
+
+    plt.close()
